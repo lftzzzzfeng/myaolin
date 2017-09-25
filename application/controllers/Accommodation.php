1b580b7f5003824d3c5fe7dd68d4ce830f94ca6e
Administrator <Administrator@IT-20170611YHIZ.lan>
2017-09-25 09:10:13 +0800

    住在瑶琳恢复

--- application/controllers/Accommodation.php
+++ application/controllers/Accommodation.php
@@ -4,6 +4,7 @@
  * User: LiuFeng
  * Date: 2017/8/30
  * Time: 9:04
+ * 住在瑶琳
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
@@ -13,6 +14,7 @@ class Accommodation extends MainController
 
     public function __construct()
     {
+
         parent::__construct();
         $this->load->model('accommodationCategoryModel');
         $this->load->model('accommodationCategoryImageModel');
@@ -24,12 +26,19 @@ class Accommodation extends MainController
 
     public function index()
     {
-        
-        
-        $this->content['pageTitle'] = '住在瑶琳';
-
-        $content['accommodationCategories'] = $this->accommodationCategoryModel->getAllAccommodationCategoriesDetail();
-
-        $this->renderView($this->mainTemplatePath . $this->router->fetch_method(), $content);
+        $content['pageTitle'] = '住在瑶琳 - 瑶琳国家森林公园';
+        $content['accommodation'] = $this->accommodationCategoryModel->getAllAccommodationCategoriesDetail();
+        $this->load->view($this->mainTemplatePath . $this->router->fetch_method(), $content);
+        $this->load->view('main/template/footer');
+    }
+    
+    //住在瑶琳详情
+    public function accommodationinfo()
+    {
+        $id = $this->input->get('id');
+        $content['pageTitle'] = '住在瑶琳详情 - 瑶琳国家森林公园';
+        $content['accommodationinfo'] = $this->accommodationCategoryModel->getAccommodationInfo($id);
+        $this->load->view($this->mainTemplatePath . $this->router->fetch_method(), $content);
+        $this->load->view('main/template/footer');
     }
 }
