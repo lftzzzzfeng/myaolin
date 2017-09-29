50ea7d9a3a10b9bde751bd8b297bbf3d43b88d21
liufeng <lftzzzz@hotmail.com>
2017-09-29 15:21:39 +0800

    Merge branch 'master' of https://github.com/lftzzzzfeng/myaolin
    # Conflicts:
    #	application/controllers/Accommodation.php

--- application/views/main/welcome/index.php
+++ application/views/main/welcome/index.php
@@ -1,30 +1,25 @@
 <!-- banner1-->
-<div class="index_banner">
-    <div id="myCarousel" class="carousel slide">
-        <!-- 轮播（Carousel）指标 -->
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <!-- 轮播（Carousel）项目 -->
-        <div class="carousel-inner">
-            <div class="item active">
-                <img src="<?php echo base_url() ?>ui/img/mobile/index_banner.jpg" alt="First slide">
-                <div class="carousel-caption">第十六届（国际）精功模特大赛选手在瑶琳森林公园举行</div>
-            </div>
-            <div class="item">
-                <img src="<?php echo base_url() ?>ui/img/mobile/index_banner.jpg" alt="Second slide">
-                <div class="carousel-caption">标题 2</div>
-            </div>
-            <div class="item">
-                <img src="<?php echo base_url() ?>ui/img/mobile/index_banner.jpg" alt="Third slide">
-                <div class="carousel-caption">标题 3</div>
-            </div>
-        </div>
-			
+<div class="index_banner" style="margin-top:70px;">
+    <div class="container">
+    	<div class="row">
+    		<div class="span12">
+    			<div class="owl-carousel">
+    				<div class="item darkCyan">
+    					<img src="<?php echo base_url() ?>ui/img/mobile/index_banner.jpg" alt="Touch">
+    					<div class="item_txt">第十六届（国际）精功模特大赛选手在瑶琳森林公园举行</div>
+    				</div>
+    				<div class="item forestGreen">
+    					<img src="<?php echo base_url() ?>ui/img/mobile/index_banner.jpg" alt="Grab">
+    					<div class="item_txt">标题 2</div>
+    				</div>
+    				<div class="item orange">
+    					<img src="<?php echo base_url() ?>ui/img/mobile/index_banner.jpg" alt="Responsive">
+    					<div class="item_txt">标题 3</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
     </div>
-
 </div>
 <div class="index_about">
     <img class="ab_img" src="<?php echo base_url() ?>ui/img/mobile/index_a.jpg" />
@@ -37,31 +32,22 @@
     <img class="pin_i" src="<?php echo base_url() ?>ui/img/mobile/index_f.jpg"/>
 
     <div class="pin_ba">
-        <div id="myCarouse2" class="carousel slide">
-            <!-- 轮播（Carousel）指标 -->
-            <ol class="carousel-indicators ca_li">
-                <?php foreach ($scenicAreaResult['scenicAreaImages'] as $k => $v){ ?>
-                    <?php if($k == 0){ ?>
-                        <li data-target="#myCarouse2" data-slide-to="<?php echo $k; ?>" class="active2"></li>
-                    <?php }else{ ?>
-                        <li data-target="#myCarouse2" data-slide-to="<?php echo $k; ?>"></li>
-                    <?php } ?>
-                <?php } ?>
-            </ol>
-            <!-- 轮播（Carousel）项目 -->
-            <div class="carousel-inner">
-                <?php foreach ($scenicAreaResult['scenicAreaImages'] as $k => $v){ ?>
-                    <?php if($k == 0){ ?>
-                        <div class="item active">
-                            <img src="<?php echo $v['image'] ?>" alt="Third slide" style="height:280px;">
-                        </div>
-                    <?php }else{ ?>
-                        <div class="item">
-                            <img src="<?php echo $v['image'] ?>" alt="Third slide" style="height:280px;">
-                        </div>
-                    <?php } ?>
-                <?php } ?>
-            </div>
+        <div class="index_banner">
+        	<div class="container2">
+        		<div class="row">
+        			<div class="span12">
+        				<div class="owl-carousel">
+
+                        <?php foreach ($scenicAreaResult['scenicAreaImages'] as $k => $v){ ?>
+        					<div class="item">
+        						<img src="<?php echo $v['image'] ?>" alt="Touch" style="width:100%;">
+        					</div>
+        				<?php } ?>
+
+        				</div>
+        			</div>
+        		</div>
+        	</div>
         </div>
     </div>
 </div>
@@ -83,93 +69,87 @@
 <!-- banner3-->
 <div class="index_you">
     <div class="pin_ba">
-        <div id="myCarouse3" class="carousel slide">
-            <!-- 轮播（Carousel）指标 -->
-            <ol class="carousel-indicators ca_li" style="margin-bottom: -6px;">
-                <li data-target="#myCarouse3" data-slide-to="0" class="active2"></li>
-                <li data-target="#myCarouse3" data-slide-to="1"></li>
-                <li data-target="#myCarouse3" data-slide-to="2"></li>
-                <li data-target="#myCarouse3" data-slide-to="3"></li>
-                <li data-target="#myCarouse3" data-slide-to="4"></li>
-                <li data-target="#myCarouse3" data-slide-to="5"></li>
-            </ol>
-            <!-- 轮播（Carousel）项目 -->
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img src="<?php echo base_url() ?>ui/img/mobile/cy_1.jpg" />
-                    <!-- txt-->
-                    <div class="item_txt" style="top:8%;">
-                        <p class="item_txt_p1">游在瑶琳</p>
+        <div class="index_banner">
+        		<div  class="container3">
+        			<div class="owl-carousel">
+        				<div class="item">
+        					<img src="<?php echo base_url() ?>ui/img/mobile/cy_1.jpg" />
+        					<!-- txt-->
+        					<div class="item_txt" style="top:8%;">
+        						<p class="item_txt_p1">游在瑶琳</p>
 
-                        <p class="item_txt_p2"></p>
+        						<p class="item_txt_p2"></p>
 
-                        <p class="item_txt_p3">尝试一下景区的不同旅行方式吧</p>
+        						<p class="item_txt_p3" style="font-size: 13px;">尝试一下景区的不同旅行方式吧</p>
 
-                        <a href="<?php echo base_url() ?>trip/travel" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="<?php echo base_url() ?>ui/img/mobile/cy_2.jpg" />
-                    <div class="item_txt" style="top:8%;">
-                        <p class="item_txt_p1">吃在瑶琳</p>
+        						<a href="<?php echo base_url() ?>trip/travel" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
+        					</div>
+        				</div>
+        				<div class="item">
+        					<img src="<?php echo base_url() ?>ui/img/mobile/cy_2.jpg" />
+        					<div class="item_txt" style="top:8%;">
+        						<p class="item_txt_p1">吃在瑶琳</p>
 
-                        <p class="item_txt_p2"></p>
+        						<p class="item_txt_p2"></p>
 
-                        <p class="item_txt_p3">快来瞧瞧景区的风味美食</p>
+        						<p class="item_txt_p3" style="font-size: 13px;">快来瞧瞧景区的风味美食</p>
 
-                        <a href="<?php echo base_url() ?>food" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="<?php echo base_url() ?>ui/img/mobile/cy_3.jpg" />
-                    <div class="item_txt" style="top:8%;">
-                        <p class="item_txt_p1">住在瑶琳</p>
+        						<a href="<?php echo base_url() ?>food" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
+        					</div>
+        				</div>
+        				<div class="item">
+        					<img src="<?php echo base_url() ?>ui/img/mobile/cy_3.jpg" />
+        					<div class="item_txt" style="top:8%;">
+        						<p class="item_txt_p1">住在瑶琳</p>
 
-                        <p class="item_txt_p2"></p>
+        						<p class="item_txt_p2"></p>
 
-                        <p class="item_txt_p3">暂不开放</p>
+        						<p class="item_txt_p3" style="font-size: 13px;">暂不开放</p>
 
-                        <a href="<?php echo base_url() ?>accommodation" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="<?php echo base_url() ?>ui/img/mobile/cy_4.jpg" />
-                    <div class="item_txt" style="top:8%;">
-                        <p class="item_txt_p1">行在瑶琳</p>
+        						<a href="<?php echo base_url() ?>accommodation" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
+        					</div>
+        				</div>
+        				<div class="item">
+        					<img src="<?php echo base_url() ?>ui/img/mobile/cy_4.jpg" />
+        					<div class="item_txt" style="top:8%;">
+        						<p class="item_txt_p1">行在瑶琳</p>
 
-                        <p class="item_txt_p2"></p>
+        						<p class="item_txt_p2"></p>
 
-                        <p class="item_txt_p3">去景区的几种路线方式</p>
+        						<p class="item_txt_p3" style="font-size: 13px;">去景区的几种路线方式</p>
 
-                        <a href="<?php echo base_url() ?>routes" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="<?php echo base_url() ?>ui/img/mobile/cy_5.jpg" />
-                    <div class="item_txt" style="top:8%;">
-                        <p class="item_txt_p1">购在瑶琳</p>
+        						<a href="<?php echo base_url() ?>routes" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
+        					</div>
+        				</div>
+        				<div class="item">
+        					<img src="<?php echo base_url() ?>ui/img/mobile/cy_5.jpg" />
+        					<div class="item_txt" style="top:8%;">
+        						<p class="item_txt_p1">购在瑶琳</p>
 
-                        <p class="item_txt_p2"></p>
+        						<p class="item_txt_p2"></p>
 
-                        <p class="item_txt_p3">暂不开放</p>
+        						<p class="item_txt_p3" style="font-size: 13px;">暂不开放</p>
 
-                        <a href="<?php echo base_url() ?>shopping" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="<?php echo base_url() ?>ui/img/mobile/cy_6.jpg" />
-                    <div class="item_txt" style="top:8%;">
-                        <p class="item_txt_p1">娱在瑶琳</p>
+        						<a href="<?php echo base_url() ?>shopping" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
+        					</div>
+        				</div>
+        				<div class="item">
+        					<img src="<?php echo base_url() ?>ui/img/mobile/cy_6.jpg" />
+        					<div class="item_txt" style="top:8%;">
+        						<p class="item_txt_p1">娱在瑶琳</p>
 
-                        <p class="item_txt_p2"></p>
+        						<p class="item_txt_p2"></p>
 
-                        <p class="item_txt_p3">快来享受吧</p>
+        						<p class="item_txt_p3" style="font-size: 13px;">快来享受吧</p>
 
-                        <a href="<?php echo base_url() ?>entertainment" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        						<a href="<?php echo base_url() ?>entertainment" class="item_txt_btn">查看详情 &nbsp;<img style="width:20px;" src="<?php echo base_url() ?>ui/img/mobile/index_xx.png"></a>
+        					</div>
+        				</div>
+        			</div>
+
+        		</div>
+
+        	</div>
 
     </div>
 </div>
@@ -179,12 +159,12 @@
     <div class="index_zxcon">
         <?php foreach ($news as $k => $v){ ?>
             <div class="zx_left">
-                <a href="<?php echo base_url() ?>news/detail?id=<?php echo $v['id']; ?>"><img src="<?php echo $v['coverImage'];?>" style="height:160px;" /></a>
+                <a href="<?php echo base_url() ?>news/detail?id=<?php echo $v['id']; ?>"><img src="<?php echo $v['coverImage'];?>" style="width:100%;" /></a>
                 <a href="<?php echo base_url() ?>news/detail?id=<?php echo $v['id']; ?>"><p class="zx_p"><?php echo $v['title']; ?><br/><?php echo $v['description']; ?></p></a>
                 <div class="zx_bot">
                     <div class="zx_botleft">
                         <p class="l_p"><?php echo $v['publishedTimestamp']; ?></p>
-                        <p class="l_p"><img src="<?php echo base_url() ?>ui/img/mobile/eye.png" /><?php echo $v['hits']; ?>人浏览</p>
+                        <p class="l_p"><img src="<?php echo base_url() ?>ui/img/mobile/eye.png" /><?php echo $v['hits']; ?></p>
                     </div>
                     <div class="zx_botleft zx_botright"><a href="<?php echo base_url() ?>news/detail?id=<?php echo $v['id']; ?>">了解详情</a></div>
                 </div>
@@ -206,5 +186,15 @@
     $(".ca_li").on("click","li",function(){
         $(".ca_li li").removeClass("active2");
         $(this).addClass("active2");
+    });
+
+    //轮播
+    $(document).ready(function($) {
+    	$(".owl-carousel").owlCarousel({
+    	itemsMobile:[479,1],
+    	itemsTablet:[768,1],
+    	navigation:true,
+    	autoPlay:2000
+    	});
     });
 </script>
